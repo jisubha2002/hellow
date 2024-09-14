a6df249c840e16b03da6d8bae5cc95761019236e
jisubha2002 <majisubhankar2002@gmail.com>
2024-09-14 22:58:43 +0530

    Update tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,14 @@
-roll
+lass EMP:
+    def_init_(self):
+    self.roll = roll
+    self.dept = dept
+    self.sal = sal
+
+    def showDetail(self):
+        print("roll is =", self.roll)
+        print("dept is =", self.dept)
+        print("salary is =", self.sal)
+
+
+e1 = EMP("staf", "gr-b", "50000")
+e1.showDetail()
